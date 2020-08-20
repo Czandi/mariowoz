097d6e445cdeb6180c0974ab8df61c6de4d21f12
Kasprzak Dominik <44524120+Czandi@users.noreply.github.com>
2020-08-20 21:16:33 +0200

    Added arrow component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ArrowComponent } from './elements/arrow/arrow.component';
 import { NavbarComponent } from './elements/navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ServicesComponent } from './services/services.component';
 import { ContactComponent } from './contact/contact.component';
 import { FormComponent } from './contact/form/form.component';
+import { ArrowComponent } from './elements/arrow/arrow.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArrowComponent,
     NavbarComponent,
     HeaderComponent,
     AboutMeComponent,
     ServicesComponent,
     ContactComponent,
     FormComponent,
+    ArrowComponent,
   ],
   imports: [
     BrowserModule
--- src/app/elements/arrow/arrow.component.ts
+++ src/app/elements/arrow/arrow.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { identifierModuleUrl } from '@angular/compiler';
 
 @Component({
-  selector: 'app-arrow',
+  selector: 'arrow',
   templateUrl: './arrow.component.html',
   styleUrls: ['./arrow.component.sass']
 })
 export class ArrowComponent implements OnInit {
+
+  @Input() type: string;
+  @Input() element_id: string;
+  @Input() color: string;
 
   constructor() { }
 
