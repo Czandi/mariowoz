5376bd6c13ff6c89e88176635eb1568db1461e27
Kasprzak Dominik <44524120+Czandi@users.noreply.github.com>
2020-08-21 01:46:21 +0200

    Added navbar

--- src/app/elements/navbar/navbar.component.ts
+++ src/app/elements/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'navbar',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.sass']
 })
 export class NavbarComponent implements OnInit {
+
+  @Input() currentItem: string;
+  @Input() items: Array<string>;
+  @Input() color: string;
 
   constructor() { }
 
