2618403c3c611dd9656c0c7f47ec641373eb6741
Kasprzak Dominik <44524120+Czandi@users.noreply.github.com>
2020-08-22 01:54:53 +0200

    Added buttons functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
   title = 'mariowoz';
+
+  constructor(){
+    console.log('Hello')
+  }
+
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event){
+    console.log($event);
+    console.log("scrolling");
+
+    const verticalOffset = window.pageYOffset
+
+    console.log(verticalOffset)
+
+  } 
+
 }
--- src/app/elements/arrow/arrow.component.ts
+++ src/app/elements/arrow/arrow.component.ts
@@ -17,4 +17,40 @@ export class ArrowComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onClick(){
+
+    const header = document.getElementById('header');
+    const aboutMe = document.getElementById('about-me');
+    const services = document.getElementById('services');
+    const contact = document.getElementById('contact');
+
+    const elements = [header, aboutMe, services, contact];
+    var windowHeight;
+
+    if(this.type === 'up'){
+
+      windowHeight = -window.innerHeight;
+
+    }else if(this.type === 'down'){
+
+      windowHeight = window.innerHeight;
+
+    }
+    
+    for(let el of elements){
+
+      console.log(el);
+
+      const style = window.getComputedStyle(el);
+      const property = style.getPropertyValue('margin-top')
+      const propertyValue = property.substr(0, property.length-2);
+
+      const newValue = Number(propertyValue) - windowHeight;
+
+      el.style.marginTop = String(newValue) + 'px';
+
+    }
+
+  }
+
 }
