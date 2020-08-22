4c5f15ec21173843a997d3222ff3085ab4d7e184
Kasprzak Dominik <44524120+Czandi@users.noreply.github.com>
2020-08-22 13:23:26 +0200

    Revert "Added buttons functionality"
    This reverts commit 2618403c3c611dd9656c0c7f47ec641373eb6741.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-
 export class AppComponent {
   title = 'mariowoz';
-
-  constructor(){
-    console.log('Hello')
-  }
-
-  @HostListener('window:scroll', ['$event']) onScrollEvent($event){
-    console.log($event);
-    console.log("scrolling");
-
-    const verticalOffset = window.pageYOffset
-
-    console.log(verticalOffset)
-
-  } 
-
 }
