f38055bf44b2f0205efa4d849430d187f4e1e366
Kasprzak Dominik <44524120+Czandi@users.noreply.github.com>
2020-08-27 21:38:11 +0200

    Working on animations

--- src/app/contact/form/form.component.ts
+++ src/app/contact/form/form.component.ts
@@ -6,7 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.sass'],
 })
 export class FormComponent implements OnInit {
+  formBackgroundElement: any;
+  informationElement: any;
+  formElement: any;
+  arrowElement: any;
+  buttonElement: any;
+  elArray: any;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.formBackgroundElement = document.getElementById('form-background');
+
+    this.informationElement = document.getElementById('information');
+    this.formElement = document.getElementById('form');
+    this.arrowElement = document.getElementById('arrow');
+    this.buttonElement = document.getElementById('contact-me-button');
+
+    this.elArray = [
+      this.informationElement,
+      this.formElement,
+      this.arrowElement,
+      this.buttonElement,
+      this.formBackgroundElement,
+    ];
+  }
+
+  onClick() {
+    this.elArray.forEach((el) => {
+      el.classList.toggle('open');
+    });
+    this.formBackgroundElement.classList.remove('hover');
+  }
+
+  mouseEnter() {
+    if (!this.formBackgroundElement.classList.contains('open')) {
+      this.formBackgroundElement.classList.add('hover');
+    }
+  }
+
+  mouseLeave() {
+    if (!this.formBackgroundElement.classList.contains('open')) {
+      this.formBackgroundElement.classList.remove('hover');
+    }
+  }
 }
